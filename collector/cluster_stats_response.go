af0d1a7316361082c322b384cb2c6ad4a9ca340d
Buelent Zeyben <bzeyben@yahoo.com>
2018-03-07 16:11:35 -0500

    Update cluster_stats_response.go

--- collector/cluster_stats_response.go
+++ collector/cluster_stats_response.go
@@ -14,6 +14,31 @@ type ClusterStatsResponse struct {
 type ClusterStatsIndicesResponse struct {
 	Count int64 `json:"count"`
 	Shards int64 `json:"shards"`
-	
-  
+	Docs 
+}
+
+type ClusterStatsIndicesShardsResponse struct {
+	Total int64 `json:"total"`
+	Primaries int64 `json:"primaries"`
+	Replication float64 `json:"replication"`
+	Index map[string]ClusterStatsIndicesShardsIndexResponse `json:"index"`
+}
+
+type ClusterStatsIndicesShardsIndexResponse struct {
+	Min float64 `json:"min"`
+	Max float64 `json:"max"`
+	Avg float64 `json:"avg"`
+}
+
+type ClusterStatsNodeResponse struct {
+	Count ClusterStatsNodeCountResponse `json:"count"`
+	Versions []string `json:"versions"`
+}
+
+type ClusterStatsNodeCountResponse struct {
+	Total int64 `json:"total"`
+	Data int64 `json:"data"`
+	CoordinatingOnly int64 `json:"coordinating_only"`
+	Master int64 `json:"master"`
+	Ingest int64 `json:"ingest"`
 }
