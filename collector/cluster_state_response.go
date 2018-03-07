ee1ac0c9af1055c1cdc0b5c5c92fec578b79998e
Buelent Zeyben <bzeyben@yahoo.com>
2018-03-07 13:19:24 -0500

    Update cluster_state_response.go

--- collector/cluster_state_response.go
+++ collector/cluster_state_response.go
@@ -1,1 +1,16 @@
 package collector
+
+import "encoding/json"
+
+// NodeStatsResponse is a representation of a Elasticsearch Node Stats
+type ClusterStateResponse struct {
+    ClusterName string `json:"cluster_name"`
+    Version     int64 `json:"version"`
+	  Nodes       map[string]ClusterStateNodeResponse `json:"nodes"`
+    Metadata    ClusterStateMetadataResponse `json:"metadata"`
+}
+
+type ClusterStateMetadataResponse struct {
+  ClusterUUID string `json:"cluster_uuid"`
+  
+}
